d1aaef29ce03d1ea7d21abab921ec7562fa449de
Sakib Fakir <FakirSakib22232@gmail.com>
2025-07-02 21:19:29 +0600

    new cors add

--- src/app/app.ts
+++ src/app/app.ts
@@ -15,6 +15,13 @@ app.use(express.json());
 
 // cors 
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // or specify: http://localhost:5173
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(cors());
 
 
